bb7d81c61feba02a2123a7ffade0eeb52097863b
Michael Sullivan <superbossmend2@gmail.com>
2020-10-09 13:51:13 +0000

    stuff

--- catty.lua
+++ catty.lua
@@ -1,1 +1,18 @@
 print("github test file")
+
+function calcu()
+  print("1st #")
+  x1 = io.read()
+
+  print("2nd #")
+  x2 = io.read()
+
+  print("1. 1st + 2nd")
+  print("2. 1st - 2nd")
+  print("3. 1st * 2nd")
+  print("4. 1st / 2nd")
+
+  if io.read() == "1" then
+    a = x1 + x2
+  elseif
+end
